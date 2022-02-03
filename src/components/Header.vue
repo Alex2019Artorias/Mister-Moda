bf612e523eae853e95d950f190677a7c833e13c4
Alex2019Artorias <2019.alex.kim@gmail.com>
2022-02-03 17:44:54 +0200

    сделал примеры для vue и scss

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="con">
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+@use './../scss/colors';
+@use './../scss/mixins';
+
+.con {
+    height: 1000px;
+    width: 1000px;
+    background-color: colors.$red;
+}
+
+.item {
+    background-color: black;
+    border: 1px solid rgb(29, 159, 199);
+    width: 100px;
+    height: 100px;
+}
+
+@include mixins.mobile-only {
+    .con {
+        background-color: #000000;
+    }
+}
+</style>
